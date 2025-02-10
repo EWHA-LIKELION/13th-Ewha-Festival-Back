1d65a226381b6755c409ee8b3caf73e43927cbad
soli-ziyo <solac@ewha.ac.kr>
2025-02-11 03:07:40 +0900

    fix: settings/base.py 수정

--- festival/settings/base.py
+++ festival/settings/base.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    
     'accounts',
     'booths',
     'shows',
