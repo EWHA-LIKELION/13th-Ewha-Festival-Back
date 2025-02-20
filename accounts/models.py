36da1a4bb709d46f76bae892bdb87c3f17ea1fc2
jiminnimij <010709min@ehwain.net>
2025-02-20 17:27:52 +0900

    fix: user 모델 수정
    booth 정보, 스크랩 수 필드 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 #상속받아 쓸 수 있는 유저모델 :AbstractUser
 from django.contrib.auth.models import AbstractUser
+from booths.models import Booth
 
 # Create your models here.
 class User(AbstractUser):
     nickname = models.CharField(max_length=10, blank=True, null=True)
     is_booth = models.BooleanField(default=False)
     #email = models.EmailField(unique=True)  # 이메일 필드를 명시적으로 추가
+    booth = models.ForeignKey(Booth, on_delete=models.SET_NULL, related_name='manager', null=True)
+    scrap_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.nickname
