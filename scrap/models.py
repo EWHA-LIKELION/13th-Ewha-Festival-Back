279957292f875b7ea0021ed9aaf9726d90934d2b
jiminnimij <010709min@ehwain.net>
2025-02-20 18:06:44 +0900

    feat: scrap, notice entity 생성

--- scrap/models.py
+++ scrap/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from accounts.models import User
+from booths.models import Booth
 
-# Create your models here.
+class Scrap(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='scrap_user')
+    booth = models.ForeignKey(Booth, on_delete=models.CASCADE, related_name='scrap_booth')
+
+    def __str__(self):
+        return f'{self.user.nickname} - {self.booth.name} scrap'
