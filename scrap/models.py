381c95a1831df6f872e8f82970f969c532428e76
minsihihi <qkralstj@gmail.com>
2025-02-22 02:43:01 +0900

    feat: scrap 모델 추가

--- scrap/models.py
+++ scrap/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from accounts.models import User
 from booths.models import Booth
 
-class Scrap(models.Model):
+class Booth_scrap(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='scrap_user')
+    booth = models.ForeignKey(Booth, on_delete=models.CASCADE, related_name='scrap_booth')
+
+    def __str__(self):
+        return f'{self.user.nickname} - {self.booth.name} scrap'
+    
+    
+class Show_scrap(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='scrap_user')
     booth = models.ForeignKey(Booth, on_delete=models.CASCADE, related_name='scrap_booth')
 
