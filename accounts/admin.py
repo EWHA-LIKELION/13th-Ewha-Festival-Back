0ad443bf8c8997334af1fc6d01e997e2fe11b260
soli-ziyo <solac@ewha.ac.kr>
2025-02-23 17:51:38 +0900

    feat: 카카오로그인 API 구현

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,4 +6,4 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-	list_display = ['id', 'is_booth', 'username', 'nickname']
+	list_display = ['id', 'is_booth', 'username']
--- accounts/models.py
+++ accounts/models.py
@@ -5,11 +5,10 @@ from booths.models import Booth
 
 # Create your models here.
 class User(AbstractUser):
-    nickname = models.CharField(max_length=10, blank=True, null=True)
+    #nickname = models.CharField(max_length=10, blank=True, null=True)
     is_booth = models.BooleanField(default=False)
-    #email = models.EmailField(unique=True)  # 이메일 필드를 명시적으로 추가
     booth = models.ForeignKey(Booth, on_delete=models.SET_NULL, related_name='manager', null=True)
     scrap_count = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.nickname
+        return self.username
