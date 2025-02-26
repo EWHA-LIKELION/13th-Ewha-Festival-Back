f5cf0dc7fb135b0c02a5a14a387aa43f1606825e
minsihihi <qkralstj@gmail.com>
2025-02-26 19:23:33 +0900

    fix: add searchHistory model to search

--- search/models.py
+++ search/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from accounts.models import User  # 유저 모델 임포트
 
-# Create your models here.
+class SearchHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="search_history")
+    query = models.CharField(max_length=100)  # 검색어 저장
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]  # 최신 검색어 기준 정렬
