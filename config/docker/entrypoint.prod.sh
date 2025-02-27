930e37cfc61a3dfa2940c7cb5c9ec9b395ab86e3
soli-ziyo <solac@ewha.ac.kr>
2025-02-27 23:25:39 +0900

    fix:docker-compose.prod 수정

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
-
-echo "Waiting for database..."
-until mysqladmin ping -h db --silent; do
-  sleep 2
-  echo "Waiting for MySQL to be ready..."
-done
-
-echo "Database is ready! Running migrations..."
+# python manage.py collectstatic --no-input echo "Apply database migrations" python manage.py migrate
+python manage.py collectstatic --no-input
 python manage.py migrate contenttypes --fake-initial
 python manage.py migrate auth --fake-initial
 python manage.py migrate accounts --fake-initial
 python manage.py migrate admin --fake-initial
 python manage.py migrate sessions --fake-initial
 python manage.py migrate --fake-initial
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-echo "Starting Gunicorn..."
-exec gunicorn festival.wsgi:application --bind 0.0.0.0:8000
+exec "$@"
