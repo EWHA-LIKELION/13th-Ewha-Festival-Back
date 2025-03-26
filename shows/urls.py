311f958ac4013811f77a455ef42ce9282dfad8a5
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-03-26 16:14:15 -0700

    Update urls.py

--- shows/urls.py
+++ shows/urls.py
@@ -5,7 +5,7 @@ app_name = 'shows'
 
 urlpatterns = [
     path('', ShowListView.as_view(), name='show_list'), 
-    path('counts',ShowCountView.as_view()),
+    path('counts/',ShowCountView.as_view()),
     path('notices/<int:booth_id>/', ShowNoticeView.as_view(), name='show-notices'),
     path('guestbooks/<int:booth_id>/', ShowGuestBookView.as_view(), name='show-guestbooks'),
     path('<int:booth_id>/', ShowPatchView.as_view(), name='show-detail'),
