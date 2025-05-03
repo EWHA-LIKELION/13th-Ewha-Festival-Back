ebc1b9cfb3be35613a4407518a5d82ff0642e3a9
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-05-03 19:34:37 +0900

    fix: 관리자 코드 조회 수정정

--- mypages/views.py
+++ mypages/views.py
@@ -1,5 +1,5 @@
 # views.py
-
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
