f0da9e06664fcee4fe430590dc7e4368f0918f5c
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-05-04 14:01:15 +0900

    feat: 상세 페이지 url 연결결

--- committeeBooth/urls.py
+++ committeeBooth/urls.py
@@ -7,5 +7,6 @@ app_name = 'committeeBooth'
 
 urlpatterns=[
   path('',CommitteeBoothListView.as_view()),
+  path('pages/<int:booth_id>/',CommitteeBoothView.as_view()),
   path('<int:booth_id>/', CommitteeBoothPatchView.as_view()),
 ]
