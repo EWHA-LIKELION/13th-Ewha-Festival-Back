352157bbd98aeb15be97fbd14657ce96ef1d7612
soli-ziyo <solac@ewha.ac.kr>
2025-05-09 22:43:40 +0900

    fix:nginx 클라이언트 최대 요청 본문 크기 수정

--- festival/settings/base.py
+++ festival/settings/base.py
@@ -212,6 +212,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'       
-
-DATA_UPLOAD_MAX_MEMORY_SIZE = 15 * 1024 * 1024
-FILE_UPLOAD_MAX_MEMORY_SIZE = 15 * 1024 * 1024
